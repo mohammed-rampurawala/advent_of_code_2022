32afe8b23b3006ad5901305fbabe0e41c713b0e9
m.rampurawala <mohammed.rampurawala@deliveryhero.com>
2022-12-10 10:45:52 +0100

    Day10 Solution

--- src/main/kotlin/day09/Day09.kt
+++ src/main/kotlin/day09/Day09.kt
@@ -7,17 +7,8 @@ fun main() {
     //L, U, R, D (Row, Column)
     val directionSteps = listOf(Pair(0, -1), Pair(-1, 0), Pair(0, 1), Pair(1, 0))
 
-    fun getDirectionPos(direction: String): Int {
-        return when (direction) {
-            "L" -> 0
-            "U" -> 1
-            "R" -> 2
-            "D" -> 3
-            else -> -1
-        }
-    }
-
     fun findTailPosition(hPos: Pair<Int, Int>, tPos: Pair<Int, Int>): Pair<Int, Int> {
+
         return when {
             abs(hPos.first - tPos.first) <= 1 && abs(hPos.second - tPos.second) <= 1 -> tPos
             abs(hPos.first - tPos.first) >= 2 && abs(hPos.second - tPos.second) >= 2 -> {
@@ -59,16 +50,17 @@ fun main() {
         }
     }
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<Movement>): Int {
         var hPos = Pair(0, 0)
         var tPos = Pair(0, 0)
         val tailTrack = mutableSetOf<Pair<Int, Int>>()
         tailTrack.addAll(listOf(tPos))
         input.forEachIndexed { _, d ->
-            val (direction, stepSize) = d.split(" ")
-            for (step in 0 until stepSize.toInt()) {
-                val dPos = getDirectionPos(direction)
-                hPos = Pair(hPos.first + directionSteps[dPos].first, hPos.second + directionSteps[dPos].second)
+            repeat(d.count) {
+                hPos = Pair(
+                    hPos.first + directionSteps[d.direction].first,
+                    hPos.second + directionSteps[d.direction].second
+                )
                 tPos = findTailPosition(hPos, tPos)
                 if (tPos.first != Int.MIN_VALUE) tailTrack.addAll(listOf(tPos))
             }
@@ -76,22 +68,18 @@ fun main() {
         return tailTrack.count()
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<Movement>): Int {
         var hPos = Pair(0, 0)
-        //Build tPos for tails
-        val tPos: MutableList<Pair<Int, Int>> = buildList { for (i in 0 until 9) add(Pair(0, 0)) }.toMutableList()
-        val endPositionTrack = mutableSetOf<Pair<Int, Int>>()
-        endPositionTrack.add(tPos[0])
-        input.forEachIndexed { _, d ->
-            val (direction, stepSize) = d.split(" ")
-            for (step in 0 until stepSize.toInt()) {
-                val dPos = getDirectionPos(direction)
-                hPos = Pair(hPos.first + directionSteps[dPos].first, hPos.second + directionSteps[dPos].second)
+        val tPos: MutableList<Pair<Int, Int>> = MutableList(9) { Pair(0, 0) }
+        val endPositionTrack = mutableSetOf(tPos.first())
+        input.forEachIndexed { _, movement ->
+            for (step in 0 until movement.count) {
+                hPos = Pair(hPos.first + directionSteps[movement.direction].first, hPos.second + directionSteps[movement.direction].second)
                 tPos[0] = findTailPosition(hPos, tPos[0])
                 for (i in 1 until 9) {
                     tPos[i] = findTailPosition(tPos[i - 1], tPos[i])
                 }
-                if (tPos[8].first != Int.MIN_VALUE) endPositionTrack.add(tPos[8])
+                if (tPos.last().first != Int.MIN_VALUE) endPositionTrack += tPos.last()
             }
 
         }
@@ -99,6 +87,25 @@ fun main() {
     }
 
     val input = readInput("day09")
-    println(part1(input))
-    println(part2(input))
+    println(part1(convertToMovement(input)))
+    println(part2(convertToMovement(input)))
+}
+
+fun getDirectionPos(direction: String): Int {
+    return when (direction) {
+        "L" -> 0
+        "U" -> 1
+        "R" -> 2
+        "D" -> 3
+        else -> -1
+    }
+}
+
+data class Movement(val direction: Int, val count: Int)
+
+fun convertToMovement(input: List<String>): List<Movement> {
+    return input.map {
+        val (direction, stepCount) = it.split(" ")
+        Movement(getDirectionPos(direction), stepCount.toInt())
+    }
 }
---
+++ src/main/kotlin/day10/Day10.kt
@@ -0,0 +1,86 @@
+package advent.day10
+
+import advent.day10.Instruction.AddX
+import advent.day10.Instruction.Noop
+import advent.readInput
+
+const val horizontalLength = 40
+const val litPixel = '#'
+fun main() {
+    fun part1(input: List<Command>): Int {
+        var X = 1
+        var cycle = 0
+        var previousMatchedCycle = 0
+        var sum = 0
+        input.forEach { cmd ->
+            repeat(cmd.instruction.cycle) {
+                cycle += 1
+                if (previousMatchedCycle == 0 && cycle == 20) {
+                    previousMatchedCycle = cycle
+                    sum += (cycle * X)
+                } else if (previousMatchedCycle + horizontalLength == cycle) {
+                    previousMatchedCycle = cycle
+                    sum += (cycle * X)
+                }
+            }
+            if (cmd.instruction is AddX) {
+                X += cmd.xCounter
+            }
+        }
+        return sum
+    }
+
+    fun part2(input: List<Command>): Int {
+        val listOfPixels = List(6) { MutableList(horizontalLength) { '.' } }
+        var x = 1
+        var cycle = 0
+        var spritePosition = resetSpritePosition(x)
+        input.forEach { cmd ->
+            repeat(cmd.instruction.cycle) {
+                val spriteIdx = cycle % horizontalLength
+                val heightIdx = cycle / horizontalLength
+                if (spritePosition[spriteIdx] == litPixel) {
+                    listOfPixels[heightIdx][spriteIdx] = litPixel
+                }
+                cycle += 1
+            }
+            if (cmd.instruction is AddX) {
+                x += cmd.xCounter
+            }
+            spritePosition = resetSpritePosition(x)
+        }
+
+        listOfPixels.forEach {
+            print(it.toString().replace(',', ' ').replace("\\s".toRegex(), ""))
+            println()
+        }
+        return 0
+    }
+
+    val input = getCommands()
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun getCommands(): List<Command> {
+    return readInput("day10").map {
+        it.split(" ")
+    }.map {
+        if (it.first() == "noop") {
+            Command(Noop, 1)
+        } else {
+            Command(AddX, it.last().toInt())
+        }
+    }
+}
+
+private fun resetSpritePosition(x: Int): MutableList<Char> {
+    return MutableList(horizontalLength) { if (it == x - 1 || it == x - 1 + 1 || it == x - 1 + 2) litPixel else '.' }
+}
+
+private data class Command(val instruction: Instruction, val xCounter: Int)
+
+private sealed class Instruction(val cycle: Int) {
+    object Noop : Instruction(1)
+    object AddX : Instruction(2)
+}
